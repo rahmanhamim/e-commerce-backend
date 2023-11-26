190572c9e31128fd5fc6c0a44fec395cfe9d9822
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-11-26 22:10:20 +0600

    express invoked

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('E-Commerce API');
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
