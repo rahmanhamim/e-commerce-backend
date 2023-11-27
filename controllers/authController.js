a25a36aa1aca5c362658a19d8febdda0dea6bed7
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-11-27 15:23:53 +0600

    jwt added to auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,24 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { createJWT } = require("../utils");
+
 const register = async (req, res) => {
-  res.send("register route");
+  const { name, email, password } = req.body;
+
+  const emailAlreadyExist = await User.findOne({ email });
+  if (emailAlreadyExist) {
+    throw new CustomError.BadRequestError("Email already exist");
+  }
+
+  // first user to register is admin
+  const role = (await User.countDocuments({})) === 0 ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -28,5 +29,17 @@ const UserSchema = new mongoose.Schema({
     enum: ["user", "admin"],
   },
 });
+
+// mongoose middleware
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// mongoose instance method
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model("User", UserSchema);
