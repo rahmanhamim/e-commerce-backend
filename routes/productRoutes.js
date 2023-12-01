a523674dae4c5e3c1e1d7a07b324233279c29552
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-12-01 16:58:49 +0600

    product route created

---
+++ routes/productRoutes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
+const {
+  getAllProducts,
+  getSingleProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+} = require("../controllers/productController");
+
+router
+  .route("/")
+  .post([authenticateUser, authorizePermissions("admin")], createProduct)
+  .get(getAllProducts);
+
+router
+  .route("/upload")
+  .post([authenticateUser, authorizePermissions("admin")], uploadImage);
+
+router
+  .route("/:id")
+  .get(getSingleProduct)
+  .patch([authenticateUser, authorizePermissions("admin")], updateProduct)
+  .delete([authenticateUser, authorizePermissions("admin")], deleteProduct);
+
+module.exports = router;
