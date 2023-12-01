8348469816b437981a1a8bd82ebdc4b9fa62303a
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-12-01 15:37:57 +0600

    product modal created

---
+++ models/Product.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please add a product name"],
+      maxlength: [50, "Name can not be more than 50 characters"],
+      minlength: [2, "Name must be at least 3 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please add a price"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Please add a description"],
+      maxlength: [1000, "Description can not be more than 1000 characters"],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpeg",
+    },
+    category: {
+      type: String,
+      required: [true, "Please add a category"],
+      enum: ["office", "kitchen", "bedroom"],
+    },
+    company: {
+      type: String,
+      required: [true, "Please add a company"],
+      enum: {
+        values: ["ikea", "liddy", "caressa", "marcos"],
+        message:
+          "{VALUE} is not supported please select from [ikea, liddy, caressa, marcos]",
+      },
+    },
+    colors: {
+      type: [String],
+      required: [true, "Please add a color"],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: [true, "Please  add a inventory"],
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
