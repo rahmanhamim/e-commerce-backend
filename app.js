6bdd1e94e7e0056e2960fa6e1bc417d6a701e169
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-12-08 15:51:49 +0600

    small fixes and project done

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 5000;
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const cors = require("cors");
+const expressMongoSanitize = require("express-mongo-sanitize");
 
 // database
 const connectDB = require("./db/connect");
@@ -25,15 +29,23 @@ const orderRouter = require("./routes/orderRoutes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(morgan("tiny"));
+// security
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 60, // 60 requests,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(expressMongoSanitize());
+// morgan is only for development
+// app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./public"));
 app.use(fileUpload());
-
-app.get("/", (req, res) => {
-  res.send("e-commerce api");
-});
 
 app.get("/api/v1", (req, res) => {
   console.log(req.signedCookies);
